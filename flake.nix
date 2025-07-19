60464c435cc797bd7a9aadbddc8905e8c50c2c4c
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2025-07-19 19:24:46 +0300

    export compile commands

--- flake.nix
+++ flake.nix
@@ -1,39 +1,63 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      autoprintf = { clangStdenv, libclang, llvm, cmake, ninja, lib }:
-         clangStdenv.mkDerivation {
-            name = "autoprintf";
-            buildInputs = [ libclang llvm ];
-            nativeBuildInputs = [ cmake ninja ];
-            src = lib.cleanSource ./.;
-          };
+      autoprintf =
+        {
+          clangStdenv,
+          libclang,
+          llvm,
+          cmake,
+          ninja,
+          lib,
+        }:
+        clangStdenv.mkDerivation {
+          name = "autoprintf";
+          buildInputs = [
+            libclang
+            llvm
+          ];
+          nativeBuildInputs = [
+            cmake
+            ninja
+          ];
+          src = lib.cleanSource ./.;
+        };
     in
     {
-      packages = forAllSystems (system:
-        {
-          autoprintf = nixpkgs.legacyPackages.${system}.callPackage autoprintf {};
-          default = self.packages.${system}.autoprintf;
-        });
+      packages = forAllSystems (system: {
+        autoprintf = nixpkgs.legacyPackages.${system}.callPackage autoprintf { };
+        default = self.packages.${system}.autoprintf;
+      });
 
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkg = self.packages.${system}.autoprintf;
         in
         {
-          autoprintf = with nixpkgs.legacyPackages.${system}; mkShell.override {
-            stdenv = pkg.stdenv;
-          } {
-            inputsFrom = [ pkg ];
-            packages = [ lldb ];
-            hardeningDisable = [ "all" ];
-          };
+          autoprintf =
+            with nixpkgs.legacyPackages.${system};
+            mkShell.override
+              {
+                stdenv = pkg.stdenv;
+              }
+              {
+                inputsFrom = [ pkg ];
+                packages = [ lldb ];
+                hardeningDisable = [ "all" ];
+                CMAKE_EXPORT_COMPILE_COMMANDS = "ON";
+              };
           default = self.devShells.${system}.autoprintf;
-        });
+        }
+      );
     };
 }
